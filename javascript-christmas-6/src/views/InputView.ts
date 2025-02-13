e9c25976cbb48b7303ce972c9e7831abb87364a1
김영은 <jessicak0828@gmail.com>
2025-02-13 12:11:34 +0900

    feat: 방문 날짜, 메뉴 입력값 받기
    InputView 에 메서드 구현

--- javascript-christmas-6/src/views/InputView.ts
+++ javascript-christmas-6/src/views/InputView.ts
@@ -1,4 +1,11 @@
-import { Console } from '../utils/missionUtils.js';
+import { BREAK_LINE, EVENT_MONTH } from '../commons/constants/index.js';
+import { Menu } from '../commons/types/service.js';
+import { Console } from '../commons/utils/missionUtils.js';
+import OutputView from './OutputView.js';
+
+const ASK_DATE = `${EVENT_MONTH}월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)`;
+const ASK_MENU = '주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)';
+
 export default class InputView {
 	private static instance: InputView;
 
@@ -10,17 +17,17 @@ export default class InputView {
 		return this.instance;
 	}
 
-	async requestDate() {
-		const dateInput = await Console.readLineAsync('12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)');
+	async requestDate(): Promise<number> {
+		const dateInput = await Console.readLineAsync(ASK_DATE);
 		// validate dateInput
-		console.log(dateInput);
 
-		return dateInput;
+		return +dateInput;
 	}
 
-	async readDate() {
-		const input = await Console.readLineAsync('12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)');
-		// ...
+	async requestMenu(): Promise<Menu> {
+		const menuInput = await Console.readLineAsync(ASK_MENU + BREAK_LINE);
+		//validate menuInput
+		OutputView.print(menuInput);
+		return [];
 	}
-	// ...
 }
