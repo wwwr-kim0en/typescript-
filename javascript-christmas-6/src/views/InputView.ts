eb0961525eba4d959e5e35740372637823ebde5c
김영은 <jessicak0828@gmail.com>
2025-02-13 16:09:08 +0900

    chore

--- javascript-christmas-6/src/views/InputView.ts
+++ javascript-christmas-6/src/views/InputView.ts
@@ -1,9 +1,8 @@
-import { BREAK_LINE, EVENT_MONTH } from '../commons/constants/index.js';
-import { Food} from '../commons/types/service.ts';
-import { convertToMenu} from '../commons/utils/convertToMenu.ts';
+import { BREAK_LINE, EVENT_MONTH } from '../commons/constants/index.ts';
+import { Food } from '../commons/types/service.ts';
+import { convertToMenu } from '../commons/utils/convertToMenu.ts';
 import { Console } from '../commons/utils/missionUtils.js';
 import Validator from '../controllers/Validator.ts';
-import OutputView from './OutputView.js';
 
 const ASK_DATE = `${EVENT_MONTH}월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)`;
 const ASK_MENU = '주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)';
