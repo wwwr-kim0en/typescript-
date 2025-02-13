4ba408734865465c0533dff81b5141fa6df08ce2
김영은 <jessicak0828@gmail.com>
2025-02-13 14:39:56 +0900

    feat: Validation Error 클래스 생성

---
+++ javascript-christmas-6/src/models/CustomError.ts
@@ -0,0 +1,9 @@
+import { ERROR_MESSAGE_PREFIX, ERROR_MESSAGE_SUFFIX } from '../commons/constants/errorMessage';
+
+// Validation Error Class
+export class ValidationError extends Error {
+	constructor(message: string) {
+		super(ERROR_MESSAGE_PREFIX + message + ERROR_MESSAGE_SUFFIX);
+		this.name = 'ValidationError';
+	}
+}
